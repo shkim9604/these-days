d520e22197afb63e9b047e24d4f382d83b8c468b
shkim9604@naver.com <shkim9604@naver.com>
2025-06-01 19:57:17 +0900

    환율날짜구하는함수추가

--- API/ExchangeRate/ExchangeRateViewModel.swift
+++ API/ExchangeRate/ExchangeRateViewModel.swift
@@ -7,8 +7,36 @@ class ExchangeRateViewModel: ObservableObject {
         exchangerate.filter { $0.cur_unit == "USD"}
     }
     
+    private func getLatestExchangeDate() -> String {
+        let now = Date()
+        let calendar = Calendar.current
+        let weekday = calendar.component(.weekday, from: now) // 1: 일요일, 2: 월요일, ..., 7: 토요일
+        let hour = calendar.component(.hour, from: now)
+        
+        var daysToSubtract = 0
+
+        switch weekday {
+        case 1: // 일요일
+            daysToSubtract = 2
+        case 2: // 월요일
+            daysToSubtract = hour < 11 ? 3 : 0
+        case 7: // 토요일
+            daysToSubtract = 1
+        default: // 화~금
+            daysToSubtract = hour < 11 ? 1 : 0
+        }
+
+        let targetDate = Calendar.current.date(byAdding: .day, value: -daysToSubtract, to: now)!
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        return formatter.string(from: targetDate)
+    }
+
+
+
     func fetchrate() {
-        let baseDate = "20250509"
+        let baseDate = getLatestExchangeDate()
         let servicekey = "apikey"
         guard let url = URL(string: "http://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=\(servicekey)&data=AP01&searchdate=\(baseDate)") else {
             print("잘못된 URL")
